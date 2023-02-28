865e713d2e127f4ecbda5125878ed3e3dcf06055
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-02-28 12:37:37 +0900

    .

--- src/js/script.js
+++ src/js/script.js
@@ -8,18 +8,34 @@
 
 
 
-const link = document.getElementById('myLink');
-const overlay = document.getElementById('overlay');
-const banner = document.getElementById('banner');
-const closeBanner = document.getElementById('closeBanner');
 
-link.addEventListener('click', function (e) {
+
+const link1 = document.getElementById('myLink');
+const link2 = document.getElementById('myLink2');
+const overlay = document.getElementById('overlay');
+const banner1 = document.getElementById('banner');
+const banner2 = document.getElementById('banner2');
+const closeBanner1 = document.getElementById('closeBanner');
+const closeBanner2 = document.getElementById('closeBanner2');
+
+link1.addEventListener('click', function (e) {
     e.preventDefault();
     overlay.style.display = 'block';
-    banner.style.display = 'block';
+    banner1.style.display = 'block';
 });
 
-closeBanner.addEventListener('click', function () {
+link2.addEventListener('click', function (e) {
+    e.preventDefault();
+    overlay.style.display = 'block';
+    banner2.style.display = 'block';
+});
+
+closeBanner1.addEventListener('click', function () {
     overlay.style.display = 'none';
-    banner.style.display = 'none';
+    banner1.style.display = 'none';
+});
+
+closeBanner2.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    banner2.style.display = 'none';
 });
